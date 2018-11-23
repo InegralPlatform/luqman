3b2d715d8500bed315737ccd5e3026e9fa431728
abobakr8911 <abobakr8911@gmail.com>
2018-11-22 23:43:40 -0800

    start work

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Luqman</title>
+        <title>  لقمان</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,15 +79,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                Luqman
+                <h3>لقمان</h3>
+                <p> أول منصة عربية حقيقية فى الوطن العربى لتعليم عن بعد</p>
                 </div>
+
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                   
                 </div>
             </div>
         </div>
